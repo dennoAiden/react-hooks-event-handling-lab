e507ab526cb4c9e5a2519ac33ef6ea78512aa022
(dennoAiden) <(kipkiruidennis25@gmail.com)>
2024-08-01 20:38:52 +0300

    Eyes on me

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,27 @@
 // Code EyesOnMe Component Here
+
+import React from 'react';
+
+function EyesOnMe() {
+  // Event handler for focus event
+  const handleFocus = () => {
+    console.log('Good!');
+  };
+
+  // Event handler for blur event
+  const handleBlur = () => {
+    console.log('Hey! Eyes on me!');
+  };
+
+  return (
+    <button 
+      onFocus={handleFocus} 
+      onBlur={handleBlur}
+    >
+      Eyes on me
+    </button>
+  );
+}
+
+export default EyesOnMe;
+
